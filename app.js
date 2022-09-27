e3d069ad0158208f91c5db1887a4134ca29926df
David Suarez <dsuarez001@att.net>
2022-09-26 20:50:20 -0400

    partial save

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // this is my function that sums two numbers
 const sum = (a,b) => {
     return a + b
+} 
+
+function fromEuroToDollar(euroAmount)
+{
+    return euroAmount * 1.2 ;
 }
 
 // just a console log for ourselves.
 console.log(sum(7,3))
+console.log(fromEuroToDollar(10));
 
 // export the function to be used on other files 
 // (similar to the keyword `export` when using webpack)
-module.exports = { sum };
+module.exports = { sum,  fromEuroToDollar};
--- test.js
+++ test.js
@@ -9,3 +9,15 @@ it('adds 14 + 9 to equal 23', () => {
     // we expect the sum of those 2 numbers to be 23
     expect(total).toBe(23);
 });
+
+it("One euro should be 1.206 dollars", function(){
+    //import the function from app.js
+    const { fromEuroToDollar } = require('./app.js')
+
+    // use the function like its suppoed to be used
+    const dollars = fromEuroToDollar(3.5)
+    
+    // this is the comparison for the unit test
+     expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+})
+

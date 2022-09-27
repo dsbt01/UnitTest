175cb3a3654051790ce0ff7c472fe4bdeecfac1d
David Suarez <dsuarez001@att.net>
2022-09-26 20:10:29 -0400

    ready to start coding

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 const { sum } = require('./app.js');
 
 // start your first test
-test('adds 14 + 9 to equal 23', () => {
+it('adds 14 + 9 to equal 23', () => {
     //inside the test we call our sum function with 2 numbers
     let total = sum(14, 9);
 
